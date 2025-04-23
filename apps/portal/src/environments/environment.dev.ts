7ca5c426b9b1518c2e7c1d726e29d40f06b9b554
Ahmed <a.nemreen@gmail.com>
2025-04-23 21:33:09 +0300

    add files to rep
    add files to rep

---
+++ apps/portal/src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://dev-api.example.com',
+  version: '1.0.0'
+}; 
---
+++ apps/portal/src/environments/environment.uat.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://uat-api.example.com',
+  version: '1.0.0'
+}; 

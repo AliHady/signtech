0e43e3d43ef1c5ad6c4af65dbb8b033909e03605
Ahmed <a.nemreen@gmail.com>
2025-05-05 07:57:20 +0300

    Enhance and add list of services service

--- apps/portal/src/app/modules/content/services/content.service.ts
+++ apps/portal/src/app/modules/content/services/content.service.ts
@@ -7,45 +7,53 @@ import { NewsResponse } from '../../home/models/news.model';
 import { VideoResponse } from '../../home/models/video.model';
 import { EventsResponse } from '../../home/models/events.model';
 import { ImageGalleryResponse } from '../../home/models/images.model';
+import { EServiceLinksResponse } from '../../eservices/models/eserviceslinks.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
-  private apiUrlNews = `${environment.contentUrl}/news`;
-  private apiUrlEvents = `${environment.contentUrl}/events`;
-  private apiUrlVideos = `${environment.contentUrl}/videolibrary`;
-  private apiUrlPhotos = `${environment.contentUrl}/photolibrary`;
+  private readonly apiEndpoints = {
+    news: `${environment.contentUrl}/news`,
+    events: `${environment.contentUrl}/events`,
+    videos: `${environment.contentUrl}/videolibrary`,
+    photos: `${environment.contentUrl}/photolibrary`,
+    eservices: `${environment.contentUrl}/eserviceslinks`
+  };
 
   private latestNewsCache = new BehaviorSubject<NewsResponse | null>(null);
 
   constructor(private cmsDataService: CmsDataService) { }
 
+  private getPaginatedData<T>(
+    endpoint: keyof typeof this.apiEndpoints,
+    pageNumber = 1,
+    pageSize = 9
+  ): Observable<T> {
+    return this.cmsDataService.getCmsPaginatedData<T>(
+      this.apiEndpoints[endpoint],
+      pageNumber,
+      pageSize
+    );
+  }
+
   getAllNews(pageNumber = 1, pageSize = 9): Observable<NewsResponse> {
-    return this.cmsDataService.getCmsPaginatedData<NewsResponse>(
-      this.apiUrlNews,
-      pageNumber,
-      pageSize
-    );
+    return this.getPaginatedData<NewsResponse>('news', pageNumber, pageSize);
   }
+
   getAllEvents(pageNumber = 1, pageSize = 9): Observable<EventsResponse> {
-    return this.cmsDataService.getCmsPaginatedData<EventsResponse>(
-      this.apiUrlEvents,
-      pageNumber,
-      pageSize
-    );
+    return this.getPaginatedData<EventsResponse>('events', pageNumber, pageSize);
   }
+
   getAllVideos(pageNumber = 1, pageSize = 9): Observable<VideoResponse> {
-    return this.cmsDataService.getCmsPaginatedData<VideoResponse>(
-      this.apiUrlVideos,
-      pageNumber,
-      pageSize
-    );
+    return this.getPaginatedData<VideoResponse>('videos', pageNumber, pageSize);
   }
+
   getAllImages(pageNumber = 1, pageSize = 9): Observable<ImageGalleryResponse> {
-    return this.cmsDataService.getCmsPaginatedData<ImageGalleryResponse>(
-      this.apiUrlPhotos,
-      pageNumber,
-      pageSize
-    );
+    return this.getPaginatedData<ImageGalleryResponse>('photos', pageNumber, pageSize);
+  }
+
+  getAllEservices(pageNumber = 1, pageSize = 9): Observable<EServiceLinksResponse> {
+    return this.getPaginatedData<EServiceLinksResponse>('eservices', pageNumber, pageSize);
   }
 } 

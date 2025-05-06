fb4bdd65664663ce70a6a0145ada6a035723c135
Ahmed <a.nemreen@gmail.com>
2025-05-06 11:25:36 +0300

    Fix en router

--- apps/portal/src/app/app.routes.ts
+++ apps/portal/src/app/app.routes.ts
@@ -9,6 +9,11 @@ export const appRoutes: Route[] = [
   {
     path: ':lang',
     children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
       ...homeRoutes,
       ...contentRoutes,
       ...eservicesRoutes,

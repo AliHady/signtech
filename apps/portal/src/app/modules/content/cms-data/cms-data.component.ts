23739c70f50cfa6e8ffb3678991103d561a365b5
Ali Abdelhady <60982629+AliHady@users.noreply.github.com>
2025-05-07 14:47:08 +0300

    fix breadcrumbes

--- apps/portal/src/app/modules/content/cms-data/cms-data.component.ts
+++ apps/portal/src/app/modules/content/cms-data/cms-data.component.ts
@@ -9,14 +9,7 @@ import { ContentService } from '../services/content.service';
 import { environment } from '../../../../environments/environment';
 import { Content } from '../models/content.model';
 import { trigger, transition, style, animate } from '@angular/animations';
-import { NavMenu } from '../../../shared/models/navmen.model';
-import { HeaderService } from '../../../shared/services/header.service';
 import { TranslateService } from '@ngx-translate/core';
-
-export interface BreadcrumbItem {
-  label: string;
-  url?: string;
-}
 
 @Component({
   selector: 'app-cms-data',
@@ -40,8 +33,6 @@ export class CMSDataComponent implements OnInit {
   loading = true;
   error = '';
   portalUrl = environment.portalUrl;
-  currentPath = '';
-  breadcrumbItem: BreadcrumbItem[] = [];
   imageToDisplay: string | null = null;
 
   constructor(
@@ -49,24 +40,21 @@ export class CMSDataComponent implements OnInit {
     private route: ActivatedRoute,
     public translationService: TranslationService,
     public translate: TranslateService,
-    private contentService: ContentService,
-    private headerService: HeaderService) { }
+    private contentService: ContentService) { }
 
   ngOnInit() {
     const fullUrl = this.router.url.split('?')[0];
     const parts = fullUrl.split('/').filter(Boolean);
-    this.currentPath = "/CMS/" + parts.slice(1).join('/') + "/";
 
     this.route.params.subscribe(params => {
       const lang = params['lang'];
       if (lang && (lang === 'en' || lang === 'ar')) {
         this.currentLang = lang;
         this.translationService.setLanguage(lang);
-        this.fetchMenuItems();
       }
     });
 
-    this.loadContent(this.currentPath);
+    this.loadContent("/CMS/" + parts.slice(1).join('/') + "/");
   }
 
   private loadContent(route: string): void {
@@ -89,103 +77,6 @@ export class CMSDataComponent implements OnInit {
     });
   }
 
-  private fetchMenuItems() {
-    console.log('fetchMenuItems')
-    this.loading = true;
-    this.error = '';
-
-    this.headerService.getNavigationMenu().subscribe({
-      next: (response: NavMenu) => {
-        this.buildBreadcrumbs(response);
-        this.loading = false;
-      },
-      error: (error: Error) => {
-        console.error('Error fetching menu items:', error);
-        this.error = 'Failed to load menu items. Please try again later.';
-        this.loading = false;
-      }
-    });
-  }
-
-  buildBreadcrumbs(data: any[]) {
-    this.breadcrumbItem = [];
-
-    this.breadcrumbItem.push({
-      url: "/" + this.currentLang + '/home',
-      label: this.translate.instant('GENERAL.HOME')
-    });
-
-    const isPathFound = this.findPath(data);
-
-    if (!isPathFound) {
-      const lastMatch = this.findNearestMatch(data, this.currentPath);
-      if (lastMatch && !this.breadcrumbItem.some(x => x.url === lastMatch.Url.replace('CMS', this.currentLang))) {
-        this.breadcrumbItem.push({
-          url: "/" + this.currentLang + lastMatch.Url.replace('CMS', ''),
-          label: lastMatch.Text
-        });
-      }
-    }
-
-    const lastBreadcrumbUrl = this.breadcrumbItem[this.breadcrumbItem.length - 1]?.url;
-    if (lastBreadcrumbUrl && this.currentPath.replace('CMS', this.currentLang).startsWith(lastBreadcrumbUrl)) {
-      const extraPart = this.currentPath.replace('CMS', this.currentLang).replace(lastBreadcrumbUrl, '');
-      if (extraPart) {
-        const segment = extraPart.split('/').filter(x => x)[0];
-        this.breadcrumbItem.push({
-          url: "/" + this.currentLang + this.currentPath,
-          label: this.translate.instant('PRODUCTS.' + segment.toUpperCase())
-        });
-      }
-    }
-
-    // this.breadcrumbItem.reverse();
-  }
-
-  private findNearestMatch(nodes: any[], path: string): any {
-    for (const node of nodes) {
-      if (path.startsWith(node.Url)) {
-        return node;
-      }
-      if (node.Items && node.Items.length) {
-        const result = this.findNearestMatch(node.Items, path);
-        if (result) return result;
-      }
-    }
-    return null;
-  }
-
-  // buildBreadcrumbs(data: any[]) {
-  //   console.log('buildBreadcrumbs')
-  //   this.breadcrumbItem = [];
-  //   this.breadcrumbItem.push({ url: "/" + this.currentLang + '/home', label: this.translate.instant('GENERAL.HOME') });
-  //   this.findPath(data);
-  //   // this.breadcrumbItem.reverse();
-  // }
-
-  private findPath(nodes: any[]): boolean {
-    for (const node of nodes) {
-      if (this.currentPath.startsWith(node.Url)) {
-        this.breadcrumbItem.push({
-          url: "/" + this.currentLang + node.Url.replace('/CMS', ''),
-          label: node.Text
-        });
-
-        if (node.url === this.currentPath) {
-          return true;
-        }
-      }
-
-      if (node.Items && node.Items.length) {
-        if (this.findPath(node.Items)) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
   ngAfterViewInit() {
     this.attachLinkHandlers();
   }
@@ -200,7 +91,7 @@ export class CMSDataComponent implements OnInit {
           this.navigate(href);
         });
       });
-    }, 1000);
+    }, 500);
   }
 
   navigate(url: string | null) {
--- apps/portal/src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ apps/portal/src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslationService } from '@nimic/translations';
+import { HeaderService } from '../../services/header.service';
+import { NavMenu } from '../../models/navmen.model';
 
 export interface BreadcrumbItem {
   label: string;
@@ -15,8 +19,135 @@ export interface BreadcrumbItem {
 })
 export class BreadcrumbsComponent implements OnInit {
   @Input() items: BreadcrumbItem[] = [];
+  list: BreadcrumbItem[] = [];
+  currentLang = 'ar';
+  currentPath = '';
+  loading = true;
+  error = '';
+
+  constructor(
+    private router: Router,
+    public translationService: TranslationService,
+    public translate: TranslateService,
+    private headerService: HeaderService) { }
+
 
   ngOnInit() {
-    //console.log('Breadcrumb Items:', this.items);
+    const fullUrl = this.router.url.split('?')[0];
+    const parts = fullUrl.split('/').filter(Boolean);
+    const lang = parts[0];
+    if (lang && (lang === 'en' || lang === 'ar')) {
+      this.currentLang = lang;
+      this.currentPath = decodeURIComponent(fullUrl.replace('/' + lang, ''));
+      this.translationService.setLanguage(lang);
+      this.fetchMenuItems();
+    }
+  }
+
+  private fetchMenuItems() {
+    this.loading = true;
+    this.error = '';
+    this.headerService.getNavigationMenu().subscribe({
+      next: (response: NavMenu) => {
+        this.buildBreadcrumbs(response);
+        this.loading = false;
+      },
+      error: (error: Error) => {
+        console.error('Error fetching menu items:', error);
+        this.error = 'Failed to load menu items. Please try again later.';
+        this.loading = false;
+      }
+    });
+  }
+
+  buildBreadcrumbs(data: any[]) {
+    this.list = [];
+
+    this.list.push({
+      url: '/home',
+      label: this.translate.instant('GENERAL.HOME')
+    });
+
+    const isPathFound = this.findPath(data);
+    if (!isPathFound) {
+      const lastMatch = this.findNearestMatch(data, this.currentPath);
+      if (lastMatch && !this.list.some(x => x.url === lastMatch.Url)) {
+        this.list.push({
+          url: lastMatch.Url,
+          label: lastMatch.Text
+        });
+      }
+    }
+
+    const lastBreadcrumbUrl = this.list[this.list.length - 1]?.url;
+    const currentSegments = this.currentPath.split('/').filter(x => x);
+    const translationPrefix = currentSegments.length > 0 ? currentSegments[0].toUpperCase() : 'GENERAL';
+
+    if (lastBreadcrumbUrl && this.currentPath.startsWith(lastBreadcrumbUrl)) {
+      const remainingPath = this.currentPath.replace(lastBreadcrumbUrl.replace('/' + this.currentLang, ''), '');
+      const remainingSegments = remainingPath.split('/').filter(x => x);
+
+      if (remainingSegments.length) {
+        let accumulatedPath = lastBreadcrumbUrl;
+
+        for (const segment of remainingSegments) {
+          accumulatedPath += '/' + segment;
+          if (!this.list.some(x => x.url === accumulatedPath)) {
+            if (translationPrefix !== 'MEDIACENTER') {
+              this.list.push({
+                url: accumulatedPath,
+                label: this.translate.instant(translationPrefix + '.' + segment.toUpperCase())
+              });
+            }
+            else {
+              this.list.push({
+                url: accumulatedPath,
+                label: segment
+              });
+            }
+          }
+        }
+      }
+    }
+
+    for (const breadcrumb of this.list) {
+      breadcrumb.url = "/" + this.currentLang + breadcrumb.url;
+    }
+  }
+
+  private findNearestMatch(nodes: any[], path: string): any {
+    for (const node of nodes) {
+      if (path.startsWith(node.Url)) {
+        return node;
+      }
+      if (node.Items && node.Items.length) {
+        const result = this.findNearestMatch(node.Items, path);
+        if (result) return result;
+      }
+    }
+    return null;
+  }
+
+  private findPath(nodes: any[]): boolean {
+    for (const node of nodes) {
+      if (this.currentPath.startsWith(node.Url)) {
+        this.list.push({
+          url: node.Url,
+          label: node.Text
+        });
+
+        if (node.Url === this.currentPath) {
+          return true;
+        }
+      }
+
+      if (node.Items && node.Items.length) {
+        if (this.findPath(node.Items)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 } 

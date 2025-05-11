7246e03decc9ca8fd8f7ae27040eccc9e0c4165a
Ahmed <a.nemreen@gmail.com>
2025-05-11 13:03:41 +0300

    Fixes

--- apps/portal/src/app/modules/home/marsad/marsad.component.ts
+++ apps/portal/src/app/modules/home/marsad/marsad.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-marsad',
+  selector: 'app-marsad-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule, RouterModule],
   templateUrl: './marsad.component.html',
   styleUrls: ['./marsad.component.scss']
 })
--- apps/portal/src/app/modules/marsad/marsad-home/marsad-home.component.ts
+++ apps/portal/src/app/modules/marsad/marsad-home/marsad-home.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from '../../../shared/layouts/header/header.component
 import { FooterComponent } from '../../../shared/layouts/footer/footer.component';
 
 @Component({
-  selector: 'app-marsad-home',
+  selector: 'app-marsad',
   standalone: true,
   imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './marsad-home.component.html',

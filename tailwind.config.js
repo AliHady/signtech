e9951a6d7397480bfe7a4fd07cfaf50aca094974
Ali Abdelhady <60982629+AliHady@users.noreply.github.com>
2025-06-14 17:44:26 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './apps/support-link/src/**/*.{html,ts,scss}',
+      './libs/**/*.{html,ts,scss}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    important: true, // This ensures Tailwind styles take precedence
+    corePlugins: {
+      preflight: false, // This is important for Angular
+    },
+  };
+  
